9b440e512c50c632832ae6cad4991867478b311b
Dyllan Thouvignon <dthouvignon@myges.fr>
2023-10-13 16:37:24 +0200

    added new pages in accueil

--- src/views/accueil/accueilView.php
+++ src/views/accueil/accueilView.php
@@ -15,9 +15,9 @@
             </a>
             <ul class="nav-links">
                 <li><a href="#home"><i class="fa-solid fa-house-user"></i>Home</a></li>
-                <li><a href="#about"><i class="fa-solid fa-address-card"></i>About</a></li>
-                <li><a href="#privacy"><i class="fa-solid fa-lock"></i>Privacy</a></li>
-                <li><a href="#terms"><i class="fa-solid fa-sheet-plastic"></i>Terms</a></li>
+                <li><a href="#expenses"><i class="fa-solid fa-address-card"></i>About</a></li>
+                <li><a href="#reports"><i class="fa-solid fa-lock"></i>Privacy</a></li>
+                <li><a href="#insights"><i class="fa-solid fa-sheet-plastic"></i>Terms</a></li>
                 <li><a href="#contact"><i class="fa-solid fa-address-book"></i>Contact</a></li>
                 <div class="active"></div>
             </ul>
@@ -31,7 +31,7 @@
             <p>For more information, please visit <a href=">https://feefinder.app">https://feefinder.app</a>.</p>
         </section>
 
-        <section class="about-container" id="about">
+        <section class="expenses-container" id="expenses">
             <h1>ABOUT</h1>
             <h1>FeeFinder ©</h1>
             <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
@@ -39,7 +39,7 @@
             <p>For more information, please visit <a href=">https://feefinder.app">https://feefinder.app</a>.</p>
         </section>
 
-        <section class="privacy-container" id="privacy">
+        <section class="reports-container" id="reports">
             <h1>PRIVACY</h1>
             <h1>FeeFinder ©</h1>
             <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
@@ -47,7 +47,7 @@
             <p>For more information, please visit <a href=">https://feefinder.app">https://feefinder.app</a>.</p>
         </section>
 
-        <section class="terms-container" id="terms">
+        <section class="insights-container" id="insights">
             <h1>TERMS</h1>
             <h1>FeeFinder ©</h1>
             <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
@@ -55,7 +55,15 @@
             <p>For more information, please visit <a href=">https://feefinder.app">https://feefinder.app</a>.</p>
         </section>
 
-        <section class="contact-container" id="contact">
+        <section class="settings-container" id="settings">
+            <h1>CONTACT</h1>
+            <h1>FeeFinder ©</h1>
+            <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
+            <p>It is currently in development and will be released soon.</p>
+            <p>For more information, please visit <a href=">https://feefinder.app">https://feefinder.app</a>.</p>
+        </section>
+
+        <section class="contacts-container" id="contacts">
             <h1>CONTACT</h1>
             <h1>FeeFinder ©</h1>
             <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
