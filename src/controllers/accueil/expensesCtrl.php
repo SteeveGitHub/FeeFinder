f3c0e09eb3eb01e820f66c9c8e4a586b2be8a7f9
Steeve Trincal <steeve.trincal@gmail.com>
2023-11-10 16:12:58 +0100

    added changes

--- src/controllers/accueil/expensesCtrl.php
+++ src/controllers/accueil/expensesCtrl.php
@@ -3,32 +3,7 @@ session_start();
 include(__DIR__ . '../../../utils/getTemplate.php');
 
 if (isset($_SESSION['status'])) {
-    $templateData = getTemplate("../../views/accueil/expensesView.php");
-
-    $navbar = $templateData['navbar'];
-    $content = $templateData['content'];
-    $logout = $templateData['logout'];
-
-    echo <<<HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FeeFinder ©</title>
-    <link href="../styles/index.css" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/384fab6fc8.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    $navbar
-    <main class="container">
-        $content
-    </main>
-    $logout
-</body>
-</html>
-HTML;
+    getTemplate("../../views/accueil/expensesView.php");
 } else {
     header('Location: ../verifUserSessionCtrl.php');
 }
--- src/utils/getTemplate.php
+++ src/utils/getTemplate.php
@@ -1,6 +1,5 @@
 <?php
 function getTemplate($pageType) {
-
     ob_start();
     include(__DIR__ . '/../views/navbar/navbarView.php');
     $navbar = ob_get_clean();
@@ -10,9 +9,28 @@ function getTemplate($pageType) {
     $content = ob_get_clean();
 
     ob_start();
-    include (__DIR__ . '/../views/connexion/logoutView.php');
+    include(__DIR__ . '/../views/connexion/logoutView.php');
     $logout = ob_get_clean();
 
-    return compact('navbar', 'content', 'logout');
+    echo <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FeeFinder ©</title>
+    <link href="../styles/index.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/384fab6fc8.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    $navbar
+    <main class="container">
+        $content
+    </main>
+    $logout
+</body>
+</html>
+HTML;
 }
 ?>
