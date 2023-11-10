0e45e343ce538a879311a4c1e407844859f09549
Dyllan Thouvignon <dthouvignon@myges.fr>
2023-11-10 16:15:57 +0100

    add button in expenses

--- src/views/accueil/expensesView.php
+++ src/views/accueil/expensesView.php
@@ -1,8 +1,11 @@
+<section class="expenses-container" id="expenses">
+    <h1>EXPENSES</h1>
+    <h1>FeeFinder ©</h1>
+    <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
+    <p>It is currently in development and will be released soon.</p>
+    <p>For more information, please visit <a href="https://feefinder.app">https://feefinder.app</a>.</p>
 
-        <section class="expenses-container" id="expenses">
-            <h1>EXPENSES</h1>
-            <h1>FeeFinder ©</h1>
-            <p>FeeFinder © is a simple tool to calculate the fees for your PayPal transactions.</p>
-            <p>It is currently in development and will be released soon.</p>
-            <p>For more information, please visit <a href=">https://feefinder.app">https://feefinder.app</a>.</p>
-        </section>
+</section>
+<?php
+include '../../models/visiteur/bouton.php';
+?>
