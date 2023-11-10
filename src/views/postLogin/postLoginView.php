728c84c830589463a182135d6d3dd59aff2ae969
Steeve Trincal <steeve.trincal@gmail.com>
2023-11-10 14:44:11 +0100

    before merge

--- src/views/postLogin/postLoginView.php
+++ src/views/postLogin/postLoginView.php
@@ -1,13 +1,13 @@
 <?php
-    include '../../utils/getTemplate.php';
 session_start();
+include '../../utils/getTemplate.php';
     if (isset($_SESSION['status'])) {
-        if ($_SESSION['status'] === "1"){
-            echo getTemplate('../accueil/accueilView.php');
-        } else if($_SESSION['status'] === "2"){
-            echo getTemplate('../admin/adminView.php');
-        } else if($_SESSION['status'] === "3"){
-            echo getTemplate('../comptable/comptableView.php');
+        if ($_SESSION['status'] === 1){
+            echo getTemplate("../accueil/accueilView.php");
+        } else if($_SESSION['status'] === 2){
+            echo getTemplate("../admin/adminView.php");
+        } else if($_SESSION['status'] === 3){
+            echo getTemplate("../comptable/comptableView.php");
         }
     } else {
         echo getTemplate('../accueil/accueilView.php');
