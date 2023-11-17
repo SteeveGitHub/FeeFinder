515843c535f6d139f1fe9246fdbed1f1b0a8fff7
TRINCAL <99663281+SteeveGitHub@users.noreply.github.com>
2023-11-17 16:48:44 +0100

    push

--- src/views/navbar/navbarView.php
+++ src/views/navbar/navbarView.php
@@ -7,43 +7,6 @@
     <link rel="stylesheet" type="text/css" href="../../styles/index.css">
 </head>
 <body>
-<<<<<<< HEAD
-<div class="burger-container">
-<input type="checkbox" onclick="triggerMenu()" role="button" aria-label="Display the menu" class="menu">
-<nav class="navbar-not-opened" id="navbar">
-    <div class="menu-toggle" id="menuToggle">
-        <a href="#" class="logo">
-            <img class="logo-img" src="../../assets/images/LogoFeeFinder.png" alt="">
-        </a>
-    </div>
-    <ul class="nav-links">
-        <li><a href="../../controllers/accueil/accueilCtrl.php"><i class="fas fa-house-user"></i>Home</a></li>
-        <li><a href="../../controllers/accueil/expensesCtrl.php"><i class="fas fa-address-card"></i>Expenses</a></li>
-        <li><a href="../../controllers/accueil/reportCtrl.php"><i class="fas fa-lock"></i>Reports</a></li>
-        <li><a href="../../controllers/accueil/insightCtrl.php"><i class="fas fa-sheet-plastic"></i>Insights</a></li>
-        <li><a href="../../controllers/accueil/contactCtrl.php"><i class="fas fa-address-book"></i>Contact</a></li>
-        <div class="active"></div>
-    </ul>
-</nav>
-</div>
-</body>
-<script>
-    let navbar = document.getElementById("navbar");
-    let navbarClassname = "navbar-not-opened";
-
-    function triggerMenu() {
-        if (navbar.className === navbarClassname) {
-            navbar.classList.add("navbar-opened");
-            navbar.classList.remove("navbar-not-opened");
-        } else {
-            navbar.classList.add("navbar-not-opened");
-            navbar.classList.remove("navbar-opened");
-        }
-    }
-</script>
-</html>
-=======
-<<<<<<< HEAD
 <nav class="navbar">
     <a href="#" class="logo">
         <img class="logo-img" src="../../assets/images/LogoFeeFinder.png" alt="">
@@ -57,7 +20,6 @@
         <div class="active"></div>
     </ul>
 </nav>
-=======
     <nav class="navbar">
         <a href="#" class="logo">
             <img class="logo-img" src="../../assets/images/LogoFeeFinder.png" alt="">
@@ -72,6 +34,5 @@
             <div class="active"></div>
         </ul>
     </nav>
->>>>>>> refs/remotes/origin/backv2.2
 </body>
->>>>>>> 66efb8bc3d403e0119d3ee1ef5627efcc8b7bbe6
+</html>
