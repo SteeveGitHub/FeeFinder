54914f1c61bdec174ef35362c03b6cc1edf881cc
guts-r <145131690+guts-r@users.noreply.github.com>
2023-11-17 15:53:58 +0100

    formulaire php mail

--- src/views/navbar/navbarView.php
+++ src/views/navbar/navbarView.php
@@ -18,7 +18,7 @@
         <li><a href="../../controllers/accueil/expensesCtrl.php"><i class="fa-solid fa-address-card"></i>Expenses</a></li>
         <li><a href="../../controllers/accueil/reportCtrl.php"><i class="fa-solid fa-lock"></i>Reports</a></li>
         <li><a href="../../controllers/accueil/insightCtrl.php"><i class="fa-solid fa-sheet-plastic"></i>Insights</a></li>
-        <li><a href="../../controllers/accueil/contactCtrl.php"><i class="fa-solid fa-address-book"></i>Contact</a></li>
+        <li><a href="../../controllers/accueil/contactView.php"><i class="fa-solid fa-address-book"></i>Contact</a></li>
         <div class="active"></div>
     </ul>
 </nav>
