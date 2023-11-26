02b6fe61b2f4df8c9696a287477f53b4de6dac50
Nicolas1904 <nicolas.ryckelynck@epitech.eu>
2023-11-26 23:55:33 +0100

    Add - Update visiteur fiche

--- src/views/frais/detailsFicheFrais.php
+++ src/views/frais/detailsFicheFrais.php
@@ -23,6 +23,8 @@ $fiche = $query->fetch(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <title>Détails et Modification de la Fiche</title>
+    <link href="../../styles/index.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 
 <body>
@@ -33,42 +35,149 @@ $fiche = $query->fetch(PDO::FETCH_ASSOC);
 
     <?php if ($table === 'frais') : ?>
         <h2>Informations pour la fiche "Frais"</h2>
-        <form action="updateFiche.php" method="post">
-            <input type="hidden" name="table" value="<?php echo $table; ?>">
-            <input type="hidden" name="id" value="<?php echo $fiche['id']; ?>">
+        <div id="forfaitFormContainer">
+            <h2>Fiche de frais</h2>
 
-            <label for="date_debut">Date de Début:</label>
-            <input type="text" name="date_debut" value="<?php echo $fiche['date_debut']; ?>">
+            <form id="forfaitForm" method="POST" action="updateFiche.php">
+                <input type="hidden" name="table" value="<?php echo $table; ?>">
+                <input type="hidden" name="id" value="<?php echo $fiche['id']; ?>">
 
-            <label for="total_night_price">Prix total (€):</label>
-            <input type="text" name="total_night_price" value="<?php echo $fiche['total_night_price']; ?>">
+                <label for="date">Date de début:</label>
+                <input type="date" id="date" name="date" required value="<?php echo date('Y-m-d', strtotime($fiche['date_debut'])); ?>"><br><br>
 
-            <label for="night_quantity">Quantité:</label>
-            <input type="text" name="night_quantity" value="<?php echo $fiche['night_quantity']; ?>">
+                <div class="hebergement">
+                    <h2>Hébergements</h2>
+                    <p id="nuitInfo">Informations: Nous prenons en charge 50€/nuit maximum</p>
+                    <input type="text" name="priceNight" placeholder="Prix total" value="<?php echo $fiche['total_night_price']; ?>" />
+                    <input type="number" name="numberNight" placeholder="Nombre de nuits" value="<?php echo $fiche['night_quantity']; ?>" />
+                </div>
 
-            <!-- Ajoutez d'autres champs si nécessaire -->
+                <div class="repas">
+                    <h2>Repas</h2>
+                    <p id="repasInfo">Informations: Nous prenons en charge 10€/repas maximum</p>
+                    <input type="text" name="priceMeal" placeholder="Prix Total" value="<?php echo $fiche['total_meal_price']; ?>" />
+                    <input type="number" name="numberMeal" placeholder="Nombre de repas" value="<?php echo $fiche['meal_quantity']; ?>" />
+                </div>
 
-            <input type="submit" value="Modifier">
-        </form>
+                <div class="trajet">
+                    <h2>Trajets</h2>
+                    <p id="transportInfo">Informations: Nous prenons en charge 50€/jour maximum</p>
+                    <label for="cars">Voitures</label>
+                    <input type="checkbox" name="cars" <?php echo $fiche['transport_type'] === 'voiture' ? 'checked' : ''; ?> />
+                    <label for="transports">Transports</label>
+                    <input type="checkbox" name="transports" <?php echo $fiche['transport_type'] === 'transport' ? 'checked' : ''; ?> />
+
+                    <div class="cars-container" style="<?php echo $fiche['transport_type'] === 'voiture' ? 'display: block;' : 'display: none;'; ?>">
+                        <p>Frais kilométriques</p>
+                        <input type="number" name="km" placeholder="Nombre de KM" value="<?php echo $fiche['km']; ?>" />
+                        <br />
+                    </div>
+
+                    <div class="transports-container" id="transports-container" style="<?php echo $fiche['transport_type'] === 'transport' ? 'display: block;' : 'display: none;'; ?>">
+                        <label for="transportType">Transports</label>
+                        <select id="transportType" name="transport">
+                            <option value="">-- Choisir --</option>
+                            <option value="train" <?php echo $fiche['transport_type'] === 'train' ? 'selected' : ''; ?>>Train</option>
+                            <option value="bus" <?php echo $fiche['transport_type'] === 'bus' ? 'selected' : ''; ?>>Bus</option>
+                            <option value="taxi" <?php echo $fiche['transport_type'] === 'taxi' ? 'selected' : ''; ?>>Taxi</option>
+                            <option value="metro&tram" <?php echo $fiche['transport_type'] === 'metro&tram' ? 'selected' : ''; ?>>Métro / Tramway</option>
+                        </select><br><br>
+                        <br />
+                    </div>
+                </div>
+
+                <input type="submit" value="Envoyer" name="forfaitSubmit">
+            </form>
+
+        </div>
 
     <?php elseif ($table === 'hors_forfait') : ?>
-        <h2>Informations pour la fiche "Hors Forfait"</h2>
-        <form action="updateFiche.php" method="post">
-            <input type="hidden" name="table" value="<?php echo $table; ?>">
-            <input type="hidden" name="id" value="<?php echo $fiche['id']; ?>">
+        <div id="horsForfaitFormContainer" style="display: block">
+            <h2>Fiche Hors Forfait</h2>
 
-            <label for="description">Description:</label>
-            <input type="text" name="description" value="<?php echo $fiche['description']; ?>">
+            <form id="horsForfaitForm" method="POST" action="updateFiche.php">
+                <input type="hidden" name="table" value="<?php echo $table; ?>">
+                <input type="hidden" name="id" value="<?php echo $fiche['id']; ?>">
 
-            <label for="total_price">Prix Total(€):</label>
-            <input type="text" name="total_price" value="<?php echo $fiche['total_price']; ?>">
+                <label for="description">Votre hors forfait:</label>
+                <textarea rows="5" cols="20" name="description-area"><?php echo $fiche['description']; ?></textarea>
 
-            <label for="number_days">Nombre de jours:</label>
-            <input type="text" name="number_days" value="<?php echo $fiche['number_days']; ?>">
+                <label for="totalPrice">Prix total:</label>
+                <input type="text" name="hors-forfait-prix" value="<?php echo $fiche['total_price']; ?>" />
 
-            <input type="submit" value="Modifier">
-        </form>
+                <label for="number_days">Nombre de jours :</label>
+                <input type="number" name="number_days" required value="<?php echo $fiche['number_days']; ?>" />
+
+                <label for="justificatif">Justificatif:</label>
+                <input type="file" name="justificatif" accept=".pdf" />
+
+                <input type="submit" value="Envoyer" name="horsForfaitSubmit">
+            </form>
+
+        </div>
     <?php endif; ?>
+
+    <script>
+        function handleCheckboxClick(clickedCheckbox) {
+            // Get the checkbox elements
+            var carsCheckbox = document.getElementsByName('cars')[0];
+            var transportsCheckbox = document.getElementsByName('transports')[0];
+
+            // Get the container elements
+            var carsContainer = document.querySelector('.cars-container');
+            var transportsContainer = document.querySelector('.transports-container');
+
+            // Check if the containers exist before accessing their properties
+            if (carsContainer && transportsContainer) {
+                // Hide both containers initially
+                carsContainer.style.display = 'none';
+                transportsContainer.style.display = 'none';
+
+                // Determine which checkbox was clicked
+                if (clickedCheckbox === carsCheckbox && carsCheckbox.checked) {
+                    // If "Voitures" is clicked, show the cars container and hide the transports container
+                    carsContainer.style.display = 'block';
+                    transportsContainer.style.display = 'none';
+                } else if (clickedCheckbox === transportsCheckbox && transportsCheckbox.checked) {
+                    // If "Transports" is clicked, show the transports container and hide the cars container
+                    transportsContainer.style.display = 'block';
+                    carsContainer.style.display = 'none';
+                }
+            }
+        }
+
+        // Attach the handleCheckboxClick function to the click events of both checkboxes
+        document.getElementsByName('cars')[0].addEventListener('click', function() {
+            handleCheckboxClick(document.getElementsByName('cars')[0]);
+        });
+        document.getElementsByName('transports')[0].addEventListener('click', function() {
+            handleCheckboxClick(document.getElementsByName('transports')[0]);
+        });
+
+        function updateFormValues(type, elementId) {
+            $.ajax({
+                url: 'getDynamicValues.php',
+                method: 'GET',
+                data: {
+                    type: type
+                },
+                dataType: 'json',
+                success: function(response) {
+                    $('#' + elementId).html('Informations: Nous prenons en charge ' + response.montant +
+                        '€ maximum selon le nombre de jours ou repas');
+                },
+                error: function(error) {
+                    console.error('Erreur lors de la récupération des valeurs :', error);
+                }
+            });
+        }
+        updateFormValues('repas', 'repasInfo');
+        updateFormValues('nuit', 'nuitInfo');
+        updateFormValues('transport', 'transportInfo');
+        document.getElementById('forfaitFormContainer').style.display = 'block';
+        document.getElementById('horsForfaitFormContainer').style.display = 'block';
+    </script>
+
 </body>
 
 </html>
